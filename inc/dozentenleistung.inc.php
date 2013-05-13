7f18837e05f364ccbe8b1779ad035572f0102cc5
froehr <f.roehr@uni-muenster.de>
2013-05-13 17:39:53 +0200

    dropdown eingefügt

--- inc/dozentenleistung.inc.php
+++ inc/dozentenleistung.inc.php
@@ -8,11 +8,10 @@ $dozent = query("SELECT * FROM `dozent` ORDER BY `dozent_name` ASC");
 if (mysql_num_rows($dozent) != 0) {
 		while ($row = mysql_fetch_object($dozent)) {
 			$tpl = copy_code("dozent");
-                        
                         $tpl = tpl_replace_once("dozentname", $row->dozent_name);
-                        
-                        
-
+			
+			$tpl = copy_code("option");
+                        $tpl = tpl_replace_once("dozentoption", $row->dozent_name);
 		}
 }
 
